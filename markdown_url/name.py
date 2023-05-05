0a4627883bad251203719b649b424a55c46e821e
zhangzhiyu <erbiaoger@gmail.com>
2023-05-05 15:26:31 +0800

    整理一下

--- markdown_url/name.py
+++ markdown_url/name.py
@@ -1,9 +1,20 @@
 import glob
+import os
 
 url = '![{}](https://raw.githubusercontent.com/erbiaoger/PicGo/main/岩石标本/{})'
 
-with open("name.txt", 'w') as f:
-    for file in glob.glob('*.tiff'):
+cwd = os.getcwd()
+# 获取上一级目录的路径
+file_dir = os.path.abspath(os.path.join(cwd, ".."))
+
+# 获取最后一个目录名字
+# dirname = os.path.basename(os.path.normpath(path))
+
+os.chdir(os.path.join(file_dir, '岩石标本'))
+
+
+with open(os.path.join(cwd, "name.txt"), 'w') as f:
+    for file in glob.glob('*.png'):
         name, _ = file.split('.')
         f.write(url.format(name, file))
         f.write('\n')
